cc3768176b0da27eade11bfd6ed67c6d7eeb9235
mhpoblet <mhpoblete@ucdavi.sedu>
2018-07-06 13:37:33 -0700

    step 2

--- src/app/hello.js
+++ src/app/hello.js
@@ -1,15 +1,19 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ngMaterial']);
 
 var MyController = function ($scope, $http, $log) {
-  $http({
-    method: 'GET',
-    url: 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=5d5c6808bf9f24ea03300fe064f995a2'
-  }).then(function (response) {
-    $scope.WeatherO = response.data;
-  },
-   function (response) {
-     $log.log(response);
+  $http.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=5d5c6808bf9f24ea03300fe064f995a2').then(function (response) {
+       $scope.WeatherO = response.data;
    });
+
+//  $http({
+//    method: 'GET',
+//    url: 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=5d5c6808bf9f24ea03300fe064f995a2'
+//  }).then(function (response) {
+//    $scope.WeatherO = response.data;
+//  },
+//   function (response) {
+//     $log.log(response);
+//   });
 };
 
 module.exports = {
